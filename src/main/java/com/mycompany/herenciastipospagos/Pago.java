ac0544f489635f52facd531a6f7b8ad031813955
rgodinezp2 <rgodinezp2@miumg.edu.gt>
2023-07-26 02:03:03 -0600

    Error ...

--- src/main/java/com/mycompany/herenciastipospagos/Pago.java
+++ src/main/java/com/mycompany/herenciastipospagos/Pago.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.herenciastipospagos;
 
-/**
- *
- * @author ASUS
- */
+import java.time.LocalDate;
+
+
 public class Pago {
+    protected int monto;
+    protected String persona;
+    protected LocalDate fecha;
+    protected String metodoPago;
+   
+    public Pago(int monto, String persona, LocalDate fecha, String metodoPago){
+    this.monto = monto;
+    this.persona = persona;
+    this.fecha = fecha;
+    this.metodoPago = metodoPago;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+   
     
+    
+    
+    public void Pagar(){    
+        System.out.print("Te pago...");
+    }
 }
